12d16d60c70468f6534457c2f2cc5f0b6504b5a0
LVasacos <2067728219@qq.com>
2026-04-27 18:16:15 +0800

    6.1

--- 树转二叉树-2026-4-27.c
+++ 树转二叉树-2026-4-27.c
@@ -43,7 +43,7 @@ BiTNode* transform(CSNode *root);
 
 /*
 思路：队列读取普通树，原来的兄弟节点放右边，父子节点放左边。
-先入队根节点，出队一个节点，将所有它的children放在该节点的左节点，上一个节点的右节点，然后把所有children入队，最后出队一个节点，以此循环。
+先入队根节点结构，出队一个结构，将所有它的原树的children数据录入新创建的二叉树节点，右串联到上一个节点的左节点，然后把所有children结构入队，以此循环。
 
 */
 
@@ -52,7 +52,14 @@ BiTNode* transform(CSNode *root);
 #include "bitree.h" //请不要删除，否则检查不通过
 #include <stdbool.h>
 
+typedef struct {//创建配对结构体，方便寻找父节点;
+        CSNode* csNode;   // 原树节点
+        BiTNode* biNode;  // 对应的二叉树节点
+    } NodePair; 
+    
+
 BiTNode* transform(CSNode *root){
+    if(root==NULL){return NULL;}
     BiTNode* biRoot = (BiTNode*)malloc(sizeof(BiTNode)); //申请二叉树根节点空间
     if (biRoot == NULL) return NULL;   //初始化二叉树根节点
     biRoot->data = root->data;
@@ -60,38 +67,38 @@ BiTNode* transform(CSNode *root){
     biRoot->right = NULL;
     
     Queue* q=create_queue(); //创建队列
-    if(q==NULL){return false;}
+    if(q==NULL){return NULL;}
     
-
-    typedef struct {//创建配对结构体，方便寻找父节点;
-        CSNode* csNode;   // 原树节点
-        BiTNode* biNode;  // 对应的二叉树节点
-    } NodePair; 
-    
-    struct NodePair* pair = malloc(sizeof(struct NodePair));//录入根节点结构
-    pair->csNode=root;
-    pair->biNode=biRoot;
-    add_queue(q, pair); //原始树根节点结构入队
+    NodePair* rootpair = malloc(sizeof(NodePair));//录入根节点结构
+    rootpair->csNode=root;
+    rootpair->biNode=biRoot;
+    add_queue(q, rootpair); //原始树根节点结构入队
     
     while(!is_empty_queue(q)){  //开始处理每一个队列的元素
-        void* p =del_queue(q); //出队
+        NodePair* p = (NodePair*)del_queue(q); //出队并且强制转换为NodePair类型，方便下面temp的赋值
         int k=0;
         BiTNode* temp=p->biNode;
-        while(p->csNode->children[k]!=NULL && k<=MAX_CHILDREN_NUM-1){
-            if(k==0){temp->left=p->csNode->children[k];temp=temp->left;}
-            if(k>0){temp->right=p->csNode->children[k];temp=temp->right;}
-            struct NodePair* pair = malloc(sizeof(struct NodePair));
-            pair->csNode=p->csNode->children[k];
-            pair->biNode=temp;
-            add_queue(q, pair);
+        while(k<MAX_CHILDREN_NUM && p->csNode->children[k]!=NULL){ //注意顺序
+            BiTNode* newNode = (BiTNode*)malloc(sizeof(BiTNode)); //不能直接将原树的节点给二叉树，得创建新的二叉树节点并赋值
+            newNode->data=p->csNode->children[k]->data;
+            newNode->left=NULL;
+            newNode->right=NULL;
+
+            if(k==0){temp->left=newNode; temp=temp->left;}
+            if(k>0){temp->right=newNode; temp=temp->right;}
+            NodePair* childpair = malloc(sizeof(NodePair));
+            childpair->csNode=p->csNode->children[k];
+            childpair->biNode=temp;
+            add_queue(q, childpair);
             k++;
+
         }
+        free(p);//清理内存
     }
+
+    free_queue(q);
+    return biRoot;
  
-
-
-    
-    
-
-
 }
+
+/*实际上使用双队列会更加简洁，同步存储原树节点和对应的二叉树节点，操作时进行同步操作*/
